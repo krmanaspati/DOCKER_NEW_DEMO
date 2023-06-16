b2f863f89bbc79d81c8c0db5a2f0da6273244567
krmanaspati <65116412+krmanaspati@users.noreply.github.com>
2023-06-16 11:28:44 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,7 +6,7 @@ environment {
     stages {
         stage("print") {
             steps {
-                echo 'hello test'
+                echo ${DOCKERHUB_CREDENTIALS_PSW}
             }
         }
         stage("build") {
