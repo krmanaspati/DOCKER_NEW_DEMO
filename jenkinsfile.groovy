cd401779a5df3a4db055f4ed98d003aca858d860
krmanaspati <65116412+krmanaspati@users.noreply.github.com>
2023-06-16 10:04:32 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -22,5 +22,18 @@ pipeline {
                 bat 'docker build -t docker-demo .'
             }
         }
+
+        stage("Deploy Docker File") {
+            steps {
+                script{
+                    withCredentials([string(credentialsId: 'DOCKER_HUB', variable: 'DOCKER_HUB_PWB')]) {
+   bat ' docker login -u krmanaspati@gmail.com -p ${DOCKER_HUB_PWB}'
+                        
+}
+                    bat 'docker push docker-demo'
+                }
+                    
+            }
+        }
     }
 }
